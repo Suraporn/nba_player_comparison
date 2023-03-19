c6ca8e1a02b7846da0d09141507f7ac0a75553a8
Suraporn <suraporn83@gmail.com>
2023-03-18 20:37:09 -0700

    add initial data

---
+++ dash.py
@@ -0,0 +1,15 @@
+from dash import dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([
+    html.H1('Hello Dash'),
+    html.Div([
+        html.P('Dash converts Python classes into HTML'),
+        html.P("This conversion happens behind the scenes by Dash's JavaScript front-end")
+        ])
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
